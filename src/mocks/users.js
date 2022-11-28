e6704fb3c0f2555a57b427214935adb7bba4d1f8
Eduardo <eduardo@conectra.com.br>
2022-11-28 18:32:20 -0300

    feat: adiciona arquivos no github

---
+++ src/mocks/users.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+        id: 1,
+        name: 'Eduardo'
+    },
+    {
+        id: 2,
+        name: 'Nico'
+    },
+    {
+        id: 3,
+        name: 'Evandro'
+    },
+]
